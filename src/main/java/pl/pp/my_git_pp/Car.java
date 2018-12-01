b323658f692c2627bab304f6987fbfe93cb76660
pawelhp <pawelhp@poczta.onet.pl>
2018-12-01 11:43:30 +0100

    [add] - dodanie Hello Wold

--- src/main/java/pl/pp/my_git_pp/Car.java
+++ src/main/java/pl/pp/my_git_pp/Car.java
@@ -3,6 +3,23 @@ package pl.pp.my_git_pp;
 public class Car {
     private String brant;
     private String model;
+    public String getBrant() {
+        return brant;
+    }
 
+    public void setBrant(String brant) {
+        this.brant = brant;
+    }
 
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Car(String brant) {
+        this.brant = brant;
+    }
 }
